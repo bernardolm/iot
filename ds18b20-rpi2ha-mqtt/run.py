9fbd34612580f703931858d1e6d8b8fc107ccbd5
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-17 18:35:10 +0000

    Adding try catch

--- ds18b20-rpi2ha-mqtt/run.py
+++ ds18b20-rpi2ha-mqtt/run.py
@@ -2,17 +2,19 @@ from w1thermsensor import W1ThermSensor, Unit, Sensor
 from pprint import pprint
 import time
 
-#for sensor in W1ThermSensor.get_available_sensors():
-#    pprint(sensor)
-#    pprint(sensor.__dict__)
-#    #print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
-
 sensor = W1ThermSensor(sensor_type=Sensor.DS18B20, sensor_id="020391776aaf")
 pprint(sensor)
 pprint(sensor.__dict__)
 sensor.set_resolution(9, persist=True)
 
-while True:
+def get():
     temperature_in_celsius = sensor.get_temperature()
     print("Sensor %s has temperature %.2f" % (sensor.id, temperature_in_celsius))
+
+while True:
+    try:
+        get()
+    except Exception as e:
+        print(3)
+
     time.sleep(10)
