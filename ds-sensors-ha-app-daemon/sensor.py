1c860a4cc725edb9ab9105c58d5b4d37064d0f93
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-17 20:12:11 +0000

    Adding mqtt client

--- ds-sensors-ha-app-daemon/sensor.py
+++ ds-sensors-ha-app-daemon/sensor.py
@@ -3,16 +3,34 @@ from pprint import pprint
 
 from w1thermsensor import W1ThermSensor
 
+import mqttapi as mqtt
 
-def get(sensor=None):
+
+class MyApp(mqtt.Mqtt):
+
+    def initialize(self):
+        pass
+
+
+def get(app=None, sensor=None):
     if sensor is None:
         return
 
     temperature_in_celsius = sensor.get_temperature()
     print(f"Sensor {sensor.id} has temperature {temperature_in_celsius:.4f}")
 
+    if app is None:
+        return
 
-def worker(sensor=None):
+    app.mqtt_publish(
+        topic=f"homeassistant/{sensor.id}",
+        payload=temperature_in_celsius,
+        qos=1,
+        retain=True
+    )
+
+
+def worker(app=None, sensor=None):
     if sensor is None:
         return
 
@@ -23,7 +41,7 @@ def worker(sensor=None):
 
     while True:
         try:
-            get(sensor)
+            get(app, sensor)
         except Exception as e:
             print(e)
         finally:
@@ -31,10 +49,16 @@ def worker(sensor=None):
 
 
 def main():
+    mqtt_cfg = mqtt.get_plugin_config()
+    pprint(mqtt_cfg)
+    pprint(mqtt_cfg.__dict__)
+
+    app = MyApp()
+
     for sensor in W1ThermSensor.get_available_sensors():
         pprint(sensor)
         pprint(sensor.__dict__)
-        worker(sensor)
+        worker(app, sensor)
 
 
 if __name__ == "__main__":
