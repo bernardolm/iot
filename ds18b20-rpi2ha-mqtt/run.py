d8f7650081f79b542870cd4d5a3512658b21ccca
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-17 19:33:07 +0000

    Iterating sensors

--- ds18b20-rpi2ha-mqtt/run.py
+++ ds18b20-rpi2ha-mqtt/run.py
@@ -12,17 +12,14 @@ def get(sensor=None):
     print(f"Sensor {sensor.id} has temperature {temperature_in_celsius: %.4f}")
 
 
-def main():
-    sensor = W1ThermSensor(
-        sensor_type=Sensor.DS18B20,
-        sensor_id="020391776aaf")
-    pprint(sensor)
-    pprint(sensor.__dict__)
+def worker(sensor=None):
+    if sensor is None:
+        return
 
     try:
         sensor.set_resolution(9, persist=True)
     except Exception as e:
-        print(e)
+        print(sensor.id, e)
 
     while True:
         try:
@@ -31,6 +28,17 @@ def main():
             print(e)
         finally:
             time.sleep(5)
+
+
+def main():
+    # sensor = W1ThermSensor(
+    #     sensor_type=Sensor.DS18B20,
+    #     sensor_id="020391776aaf")
+
+    for sensor in W1ThermSensor.get_available_sensors():
+        pprint(sensor)
+        pprint(sensor.__dict__)
+        worker(sensor)
 
 
 if __name__ == "__main__":
