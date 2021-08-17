2273cd0d7c8e13aa5438833aad986c42eb9b54da
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-17 14:55:30 -0300

    Update run.py

--- ds18b20-rpi2ha-mqtt/run.py
+++ ds18b20-rpi2ha-mqtt/run.py
@@ -1,8 +1,18 @@
-from w1thermsensor import W1ThermSensor, Unit
+from w1thermsensor import W1ThermSensor, Unit, Sensor
+from pprint import pprint
+import time
 
-for sensor in W1ThermSensor.get_available_sensors():
-    print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+#for sensor in W1ThermSensor.get_available_sensors():
+#    pprint(sensor)
+#    pprint(sensor.__dict__)
+#    #print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
 
-sensor = W1ThermSensor(sensor_type=Sensor.DS18B20, sensor_id="28-020391776aaf")
+sensor = W1ThermSensor(sensor_type=Sensor.DS18B20, sensor_id="020391776aaf")
+pprint(sensor)
+pprint(sensor.__dict__)
 sensor.set_resolution(9, persist=True)
-temperature_in_celsius = sensor.get_temperature()
+
+while True:
+    temperature_in_celsius = sensor.get_temperature()
+    print("Sensor %s has temperature %.2f" % (sensor.id, temperature_in_celsius))
+    time.sleep(10)
