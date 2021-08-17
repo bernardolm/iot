668d15a82b6c22249f914fd73a218605ca732121
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-17 19:07:02 +0000

    Refactoring

--- ds18b20-rpi2ha-mqtt/run.py
+++ ds18b20-rpi2ha-mqtt/run.py
@@ -2,19 +2,26 @@ from w1thermsensor import W1ThermSensor, Unit, Sensor
 from pprint import pprint
 import time
 
-sensor = W1ThermSensor(sensor_type=Sensor.DS18B20, sensor_id="020391776aaf")
-pprint(sensor)
-pprint(sensor.__dict__)
-sensor.set_resolution(4, persist=True)
+def get(sensor=None):
+    if sensor is None:
+        return
 
-def get():
     temperature_in_celsius = sensor.get_temperature()
     print("Sensor %s has temperature %.4f" % (sensor.id, temperature_in_celsius))
 
 def main():
+    sensor = W1ThermSensor(sensor_type=Sensor.DS18B20, sensor_id="020391776aaf")
+    pprint(sensor)
+    pprint(sensor.__dict__)
+
+    try:
+        sensor.set_resolution(9, persist=True)
+    except Exception as e:
+        print(e)
+
     while True:
         try:
-            get()
+            get(sensor)
         except Exception as e:
             print(e)
         finally:
