f8fcbd1e349f7edb95a5211552ad832f65bb5230
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-17 18:44:01 +0000

    Removing resolution/precision

--- ds18b20-rpi2ha-mqtt/run.py
+++ ds18b20-rpi2ha-mqtt/run.py
@@ -5,7 +5,7 @@ import time
 sensor = W1ThermSensor(sensor_type=Sensor.DS18B20, sensor_id="020391776aaf")
 pprint(sensor)
 pprint(sensor.__dict__)
-sensor.set_resolution(9, persist=True)
+sensor.set_resolution(persist=True)
 
 def get():
     temperature_in_celsius = sensor.get_temperature()
