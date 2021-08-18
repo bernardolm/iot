443f71da036ffd859b0c147fb3fd123e770a7ce2
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-18 20:05:21 -0300

    Fixing tester

--- ds-sensors-mqtt/test.sh
+++ ds-sensors-mqtt/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+[[ "$SSLEEP_BY" == "" ]] && SLEEP_BY=5
+
 function run () {
     git pull origin master > /dev/null 2>&1
     python3 init.py &
-    sleep 5
+    sleep $SLEEP_BY
     killall -9 python3
     ./test.sh
 }
