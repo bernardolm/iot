a6143c1792428b5625caeed945cdc1b1443518ec
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-08-18 17:52:34 -0300

    Fixing publisher

--- ds-sensors-mqtt/publisher.py
+++ ds-sensors-mqtt/publisher.py
@@ -10,7 +10,7 @@ class Publisher():
         broker = '192.168.15.7'
         port = 1883
         self._topic = "/python/mqtt"
-        client_id = f'python-mqtt-{random.randint(0, 1000)}'
+        client_id = 'ds-sensors-ha-app-daemon'
         self._client = mqtt_client.Client(client_id)
         self._client.connect(broker, port)
 
@@ -19,11 +19,11 @@ class Publisher():
             logging.info('publisher has nothing to do...!')
             return
 
-        logging.info(f'publisher do with {value}!')
+        logging.info(f'publishing {value}!')
 
         result = self._client.publish(self._topic, value)
         status = result[0]
         if status == 0:
-            logging.debug(f"Send `{value}` to topic `{self._topic}`")
+            logging.debug(f'sent {value} to topic {self._topic}')
         else:
-            logging.debug(f"Failed to send message to topic {self._topic}")
+            logging.debug(f'failed to send {value} to topic {self._topic}')
