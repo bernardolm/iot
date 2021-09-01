838aa0a2acc68aaacbe2de08657b738c13e8e2a0
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-09-01 20:48:10 -0300

    Adding set resolution to sensors layer

--- ds-sensors-mqtt/src/ds_sensors_mqtt/sensors.py
+++ ds-sensors-mqtt/src/ds_sensors_mqtt/sensors.py
@@ -4,4 +4,14 @@ from w1thermsensor import W1ThermSensor
 class Sensors():
 
     def list(self):
-        return W1ThermSensor.get_available_sensors()
+        sensors = []
+
+        for s in W1ThermSensor.get_available_sensors():
+            try:
+                s.set_resolution(9, persist=True)
+            except Exception as e:
+                logging.exception([s.id, e])
+
+            sensors.append(s)
+
+        return sensors
