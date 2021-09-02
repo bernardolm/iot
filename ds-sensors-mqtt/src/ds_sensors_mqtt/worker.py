3fc043b43efe031e6a1b3d1e3d24127c3b446731
Bernardo Loureiro <bernardo.lou@gmail.com>
2021-09-01 21:35:41 -0300

    Fixing publisher

--- ds-sensors-mqtt/src/ds_sensors_mqtt/worker.py
+++ ds-sensors-mqtt/src/ds_sensors_mqtt/worker.py
@@ -5,26 +5,21 @@ import time
 
 class Worker():
 
-    def __init__(self, sensor=None, publisher=None):
-        if sensor is None:
-            raise Exception('sensor is required')
-        self._sensor = sensor
-
+    def __init__(self, publisher=None):
         if publisher is None:
             raise Exception('publisher is required')
-        self._publisher = publisher
 
+        self._publisher = publisher
         self._interval = int(os.environ.get('WORKER_INTERVAL', '15'))
 
     def do(self):
         while True:
             try:
-                value = self._sensor.do()
-                self._publisher.do(value)
+                self._publisher.do()
             except Exception as e:
                 logging.exception(e)
             finally:
-                time.sleep(self._interval)
                 if os.environ.get('WORKER_RUN_ONCE') in ['true', 'True', '1']:
                     logging.info('once run, exiting')
                     break
+                time.sleep(self._interval)
